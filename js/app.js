415c14dd2d276fc67795d35b5b0af378c8d987b7
Andre Ganske <andre.g.ganske@gmail.com>
2014-07-17 21:26:39 -0300

    correcao

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ requirejs.config({
 		"jquery": "lib/jquery-2.1.1.min",
 		"jquery.bootstrap" : "lib/bootstrap.min",
 		"angular": "js/angular.min.js",
-		"underscore" : "underscore.js"
-		"parse" : "js/parse-1.2.19.min.js",
+		"underscore" : "underscore.js",
+		"parse" : "js/parse-1.2.19.min.js"
 	},
 
 	shim: {
