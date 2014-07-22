c5f2c00d068a1c5ca35ebe73cfbd2e1798806b68
Andre Ganske <andre.g.ganske@gmail.com>
2014-07-22 20:47:19 -0300

    update

--- app/main.js
+++ app/main.js
@@ -1,20 +1,63 @@
-define(["jquery.min", "jquery.countdown.min"], function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    /*var messages = require('./messages'),
-    	count = require('./countdown');
+require.config({
+    shim: {
+        'facebook' : {
+            exports: 'fb'
+        }
+    },
 
+    paths: {
+        'facebook': '//connect.facebook.net/pt_BR/all'
+    }
+})
 
-    // Load library/vendor modules using
-    // full IDs, like:
-    var print = require('print');
+require(['fb']);
 
-    print(messages.getHello());
+define(["jquery.min", "jquery.countdown.min", "bootstrap.min", "parse.min"], function (require) {
 
-    count.getTime();*/
+    Parse.initialize("co1z3OCpRS8Ue4JBeNRmWsvj2V48sfSym0kxbCmh", "JQSS4X7cFaqA9MWlu6K4pGmoN4mFzYC9SmfizSvU");
+
+    main = new Object();
 
     $(function() {
+
+        main = {
+
+            bindings: {
+                'fb-login' : ['click', 'dologin']
+            },
+
+            init: function () {
+                this.doBindings();
+            },
+
+            doBindings: function() {
+                var that = this;
+                $.each(this.bindings, function(key, obj) {
+                    var event = obj[0];
+                    var callback = obj[1];
+                    var bindName = "[data-" + key + "]"; 
+                    $("body").off(event, bindName);
+                    $("body").on(event, bindName, function(evt) {
+                        that[callback].call(that, this, evt);
+                    });
+                });
+            },
+
+            dologin: function() {
+                Parse.FacebookUtils.logIn(null, {
+                    success: function(user) {
+                        if (!user.existed()) {
+                            alert("User signed up and logged in through Facebook!");
+                        } else {
+                            alert("User logged in through Facebook!");
+                        }
+                    },
+                    error: function(user, error) {
+                        alert("User cancelled the Facebook login or did not fully authorize.");
+                    }
+                });
+            }
+        };
 
     	$('#countdown').countdown('2015/08/15 20:00:00', function (event) {
     		var months = event.strftime('%m'),
@@ -51,4 +94,6 @@ define(["jquery.min", "jquery.countdown.min"], function (require) {
     	});
     });
 
+    main.init();
+    
 });
