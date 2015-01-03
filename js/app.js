8aa9799a3f90e636d4e2b5137c5210569d0bde9a
Andre Ganske <andre.g.ganske@gmail.com>
2015-01-03 14:32:26 -0200

    inclusao do bootstrap

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@
 
 // Configure loading modules from the lib directory, except 'app' ones
 requirejs.config({
+
     baseUrl: "js/lib",
+
+    shim: {
+        "bootstrap" : { "deps" :['jquery'] }
+    },
+
     paths: {
-      "app":        "../app",
-      "jquery":     "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
-      "angular":    "//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min",
-      "parse":      "//www.parsecdn.com/js/parse-1.3.3.min"
+        "app":        "../app",
+        "jquery":     "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
+        "angular":    "//ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min",
+        "parse":      "//www.parsecdn.com/js/parse-1.3.3.min",
+        "bootstrap":  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min"
     }
 });
 
--- js/app/main.js
+++ js/app/main.js
@@ -1,14 +1,11 @@
-define(["jquery", "parse", "countdown.min"], function() {
+define(["jquery", "parse", "countdown.min", "bootstrap"], function() {
 
     Parse.initialize("co1z3OCpRS8Ue4JBeNRmWsvj2V48sfSym0kxbCmh", "JQSS4X7cFaqA9MWlu6K4pGmoN4mFzYC9SmfizSvU");
 
-    $(function() {
-        
-        new Countdown({
-            selector: '#countdown',
-            msgPattern: "Faltam {days} dias, {hours} horas, {minutes} minutos e {seconds} segundos!",
-            dateEnd: new Date('Aug 15, 2015 20:00')
-        });
+    new Countdown({
+    	selector: '#countdown',
+    	msgPattern: "Faltam {days} dias, {hours} horas, {minutes} minutos e {seconds} segundos!",
+    	dateEnd: new Date('Aug 15, 2015 20:00')
     });
 
 });
