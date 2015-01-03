d40de66e80d2ddb0f6fed9b49eb5b0fea691ddaa
Andre Ganske <andre.g.ganske@gmail.com>
2015-01-03 18:38:45 -0200

    login e signin com bootstrap e validacao de senhas

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@ requirejs.config({
     baseUrl: "js/lib",
 
     shim: {
-        "bootstrap" : { "deps" :['jquery'] }
+        "bootstrap"         : { "deps" : ["jquery"] }
     },
 
     paths: {
-        "app":        "../app",
-        "jquery":     "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
-        "angular":    "//ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min",
-        "parse":      "//www.parsecdn.com/js/parse-1.3.3.min",
-        "bootstrap":  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min"
+        "app":              "../app",
+        "jquery":           "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
+        "parse":            "//www.parsecdn.com/js/parse-1.3.3.min",
+        "bootstrap":        "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min",
+        "validator":        "//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min"
     }
 });
 
--- js/app/main.js
+++ js/app/main.js
@@ -1,4 +1,4 @@
-define(["jquery", "parse", "countdown.min", "bootstrap"], function() {
+define([ "jquery", "parse", "countdown.min", "bootstrap", "validator"], function() {
 
     Parse.initialize("co1z3OCpRS8Ue4JBeNRmWsvj2V48sfSym0kxbCmh", "JQSS4X7cFaqA9MWlu6K4pGmoN4mFzYC9SmfizSvU");
 
@@ -6,6 +6,94 @@ define(["jquery", "parse", "countdown.min", "bootstrap"], function() {
     	selector: '#countdown',
     	msgPattern: "Faltam {days} dias, {hours} horas, {minutes} minutos e {seconds} segundos!",
     	dateEnd: new Date('Aug 15, 2015 20:00')
+    });
+
+    $(function() {
+
+    	var app = {
+
+    		bindings: {
+    			"do-login"		: ['click', 'doLogin'],
+    			"do-signin"		: ['click', 'doSignin']
+    		},
+
+    		init: function() {
+    			this.doBindings();
+    			this.passwordValidator();
+    		},
+
+    		doBindings: function() {
+    			var that = this;
+    			$.each(this.bindings, function(key, obj) {
+    				var event = obj[0],
+    					callback = obj[1],
+    					bindName = '[data-' + key + ']';
+
+    				$('body').off(event, bindName);
+    				$('body').on(event, bindName, function(evt) {
+    					that[callback].call(that, this, evt);
+    				});
+    			});
+    		},
+
+    		doLogin: function() {
+    			Parse.User.logIn($scope.user.username, $scope.user.password, {
+    				success: function(retorno) {
+    					$rootScope.user = retorno;
+    					$mdDialog.hide();
+    				},
+    				error: function(user, error) {
+    					if (error.code == 101) {
+    						$('#warn-invalid-login-info').alert();
+    					} else {
+    						$('#error-undefined').alert();
+    					}
+    				}
+    			});
+    		},
+
+    		doSignin: function() {
+    			var newUser = new Parse.User();
+    			newUser.set('username', $scope.user.username);
+    			newUser.set('name', $scope.user.name);
+    			newUser.set('email', $scope.user.email);
+    			newUser.set('password',	$scope.user.password);
+
+    			newUser.signUp (null, {
+    				success: function(user) {
+    					$rootScope.user = this.user;
+    					$mdDialog.hide();
+    				},
+    				error: function(user, error) {
+    					alert("Error: " + error.code + " " + error.message);
+    				}
+    			});
+    		},
+
+    		passwordValidator: function() {
+    			$('#signin-form').bootstrapValidator( {
+    				feedbackIcons: {
+    					valid: 'glyphicon glyphicon-ok',
+    					invalid: 'glyphicon glyphicon-remove',
+    					validating: 'glyphicon glyphicon-refresh'
+    				},
+    				fields: {
+    					confirmPassword: {
+    						validators: {
+    							identical: {
+    								field: 'password',
+    								message: 'As senhas digitadas não são iguais...'
+    							}
+    						}
+    					}
+    				}
+    			});
+    		}
+
+    	};
+
+    	app.init();
+
     });
 
 });
