224c4ff45bfb21a90a24882aa14155ed760e547f
Andre Gerson Ganske <andre.ganske@fluig.com>
2015-01-13 10:43:13 -0200

    appjs fix

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var allowLogin,
-	allowSigin,
-	useAnalytics;
-
 define([
 	'angular',
 	'filters',
@@ -22,30 +18,23 @@ define([
 		]);
 
 		app.controller('servicesctrl', ['$scope', function($scope, globalConstant, globalConfig) {
-			var that = this;
-			Parse.Config.get().then(function(config) {
-				that.allowLogin = config.get("allow_login");
-				that.allowSigin = config.get("allow_sigin");
-				that.useAnalytics = config.get("use_analytics");
+			var config  = Parse.Config.get().then(function(config) {
+				return config;
 			}, function(error) {
 				var codeString = '' + error.code;
 				Parse.Analytics.track('error', { code: codeString });
-				var config = Parse.Config.current();
-
-				if (config === undefined) {
-					globalConfig.allowLogin = false;
-					globalConfig.allowSigin = false;
-					globalConfig.useAnalytics = false;
-				} else {
-					globalConfig.allowLogin = config.get("allowLogin");
-					globalConfig.allowSigin = config.get("allowSigin");
-					globalConfig.useAnalytics = config.get("useAnalytics");
-				}
+				return Parse.Config.current();
 			});
 
-			globalConfig.allowLogin = this.allowLogin;
-			globalConfig.allowSigin = this.allowSigin;
-			globalConfig.useAnalytics = this.useAnalytics;
+			if (config === undefined) {
+				globalConfig.allowLogin = false;
+				globalConfig.allowSigin = false;
+				globalConfig.useAnalytics = false;
+			} else {
+				globalConfig.allowLogin = config.get("allowLogin");
+				globalConfig.allowSigin = config.get("allowSigin");
+				globalConfig.useAnalytics = config.get("useAnalytics");
+			}
 
 			$scope.allowLogin = globalConfig.allowLogin;
 			$scope.allowSigin = globalConfig.allowSigin;
