15ce39e2c268bae792e7e627737d5c2bef97d58a
Andre Ganske <andre.g.ganske@gmail.com>
2015-01-21 02:06:09 -0200

    server config

--- app/js/directives.js
+++ app/js/directives.js
@@ -15,17 +15,46 @@ define(['angular', 'services'], function(angular, services) {
 	dir.directive('propertiesValidation', ['$rootScope', function($rootScope) {
 		return function(scope, elm, attrs) {
 
-			if ($rootScope.allowLogin) {
-				console.log('allowLogin');
-			}
+			var params = {};
 
-			if ($rootScope.allowSingin) {
-				console.log('allowSingin');
-			}
+			var promise = Parse.Config.get().then(function(config) {
+				params.allowLogin = config.get("allow_login");
+				params.allowSingin = config.get("allow_singin");
+				params.useAnalytics = config.get("use_analytics");
+			}, function(error) {
+				Parse.Analytics.track('error', { code: '' + error.code });
+				var config = Parse.Config.current();
+				if (config === undefined) {
+					params.allowLogin = false;
+					params.allowSingin = false;
+					params.useAnalytics = false;
+				} else {
+					params.allowLogin = config.get("allowLogin");
+					params.allowSingin = config.get("allowSingin");
+					params.useAnalytics = config.get("useAnalytics");
+				}
+			});
 
-			if ($rootScope.useAnalytics) {
-				console.log('useAnalytics');
-			}
+			promise.done(function() {
+				var currentUser = Parse.User.current();
+
+				$rootScope.allowSingout = currentUser ? true : false;
+				$rootScope.allowLogin = params.allowLogin ? true : false;
+				$rootScope.allowSingin = params.allowSingin ? true : false;
+				$rootScope.useAnalytics = params.useAnalytics ? true : false;
+
+				if ($rootScope.allowLogin) {
+					console.log('allowLogin');
+				}
+
+				if ($rootScope.allowSingin) {
+					console.log('allowSingin');
+				}
+
+				if ($rootScope.useAnalytics) {
+					console.log('useAnalytics');
+				}
+			});
 		};
 	}]);
 
