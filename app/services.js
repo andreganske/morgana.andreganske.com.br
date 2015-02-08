725f9f1fcc027cce35737273367a0a4e5af1a984
Andre Ganske <andre.g.ganske@gmail.com>
2015-02-08 01:21:26 -0200

    new service.js

--- app/services.js
+++ app/services.js
@@ -1,10 +1,15 @@
 'use strict';
 
-angular.module('myApp.services', []).constant('globalConstant', {
-	name: 		'Morgana e Andre',
-	version: 	'0.1'
-}).value('globalConfig', {
-	allowLogin: 	false,
-	allowSigin: 	false,
-	useAnalytics: 	false
+var service = angular.module('myApp.services');
+
+service.service('User', function() {
+
+});
+
+service.service('Guest', function() {
+
+});
+
+service.service('Gift', function() {
+
 });
