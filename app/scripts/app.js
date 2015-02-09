9b0fcb624c28f1494066ed8a96fd0b82ef261a15
Andre Ganske <andre.g.ganske@gmail.com>
2015-02-09 00:30:37 -0200

    filters and init

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,16 @@
 'use strict';
 
-var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'angular-loading-bar', 'ParseServices']);
+var myApp = angular.module('myApp', [
+	'ngRoute',
+	'ui.bootstrap',
+	'angular-loading-bar',
+	'ParseServices',
+	'myApp.filters'
+]);
 
-myApp.config(function($routeProvider) {
+myApp.config(function($routeProvider, ParseSDK) {
+
+	ParseSDK.getConfig();
 
 	$routeProvider.when('/', {
 		templateUrl : 'app/scripts/views/home.html'
@@ -14,35 +22,4 @@ myApp.config(function($routeProvider) {
 
 	$routeProvider.otherwise({redirectTo: '/'});
 
-})
-
-.run(['ParseSDK', '$rootScope', function(ParseService, $rootScope) {
-
-	var params = {};
-	var _this = this;
-
-	var promise = Parse.Config.get().then(function(config) {
-		params.allowLogin = config.get("allow_login");
-		params.allowSingin = config.get("allow_singin");
-		params.useAnalytics = config.get("use_analytics");
-	}, function(error) {
-		Parse.Analytics.track('error', { code: '' + error.code });
-		var config = Parse.Config.current();
-		if (config === undefined) {
-			params.allowLogin = false;
-			params.allowSingin = false;
-			params.useAnalytics = false;
-		} else {
-			params.allowLogin = config.get("allowLogin");
-			params.allowSingin = config.get("allowSingin");
-			params.useAnalytics = config.get("useAnalytics");
-		}
-	});
-
-	promise.done(function() {
-		$rootScope.allowLogin = params.allowLogin ? true : false;
-		$rootScope.allowSingin = params.allowSingin ? true : false;
-		$rootScope.useAnalytics = params.useAnalytics ? true : false;
-	});
-	
-}]);
+});
--- app/scripts/services/ParseServices.js
+++ app/scripts/services/ParseServices.js
@@ -2,10 +2,42 @@
 
 angular.module('ParseServices', [])
 
-.factory('ParseSDK', function() {
+.factory('ParseSDK', function($rootScope) {
 
 	Parse.initialize("co1z3OCpRS8Ue4JBeNRmWsvj2V48sfSym0kxbCmh", "JQSS4X7cFaqA9MWlu6K4pGmoN4mFzYC9SmfizSvU");
 
-	return {};
+	var service = {
+		getConfig: function() {
+			
+			var params = {};
+			var _this = this;
+
+			var promise = Parse.Config.get().then(function(config) {
+				params.allowLogin = config.get("allow_login");
+				params.allowSingin = config.get("allow_singin");
+				params.useAnalytics = config.get("use_analytics");
+			}, function(error) {
+				Parse.Analytics.track('error', { code: '' + error.code });
+				var config = Parse.Config.current();
+				if (config === undefined) {
+					params.allowLogin = false;
+					params.allowSingin = false;
+					params.useAnalytics = false;
+				} else {
+					params.allowLogin = config.get("allowLogin");
+					params.allowSingin = config.get("allowSingin");
+					params.useAnalytics = config.get("useAnalytics");
+				}
+			});
+
+			promise.done(function() {
+				$rootScope.allowLogin = params.allowLogin ? true : false;
+				$rootScope.allowSingin = params.allowSingin ? true : false;
+				$rootScope.useAnalytics = params.useAnalytics ? true : false;
+			});
+		}
+	};
+
+	return service;
 
 });
