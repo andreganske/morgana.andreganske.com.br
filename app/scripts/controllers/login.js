c5be4fe3842bca24734e768da2a61585d0cd96cc
Andre Ganske <andre.g.ganske@gmail.com>
2015-02-09 01:05:09 -0200

    login modal

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -17,10 +17,59 @@ angular.module('myApp')
 			controller: 'ModalController',
 			size: size
 		});
+
+		modalInstance.result.then(function () {
+			if ($rootScope.logged) {
+				$location.path('/guest');
+			}
+		});
 	};
 
 	$scope.logoff = function() {
 		Parse.User.logOut();
 		$location.path('/');
 	};
-}]);
+}])
+
+.controller('ModalController', function($rootScope, $scope, $modalInstance) {
+
+	$scope.login = function () {
+		Parse.User.logIn($scope.email, $scope.password, {
+			success: function(retorno) {
+				$rootScope.logged = true;
+				$modalInstance.close();
+			},
+			error: function(user, error) {
+				if (error.code == 101) {
+					//app.showAlert(1);
+				} else {
+					//$('#error-undefined').alert();
+				}
+			}
+		});
+	};
+
+	$scope.create = function () {
+		var newUser = new Parse.User();
+
+		newUser.set('username', $scope.email);
+		newUser.set('email', 	$scope.email);
+		newUser.set('password',	$scope.password);
+		newUser.set('fullname', $scope.fullname);
+
+		newUser.signUp (null, {
+			success: function(user) {
+				$modalInstance.close();
+			},
+			error: function(user, error) {
+				//$scope.alerts.push({type: 'danger', msg: "Error: " + error.code + " " + error.message});
+				alert("Error: " + error.code + " " + error.message);
+			}
+		});
+	};
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+	
+});
